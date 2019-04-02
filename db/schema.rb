306d89f682a226cd22b90362c798edc8b5256cf1
frank zhu <qf.zhu.frank@gmail.com>
2019-04-02 16:06:59 +0800

    devise deleted and user model updated

--- db/schema.rb
+++ db/schema.rb
@@ -44,19 +44,11 @@ ActiveRecord::Schema.define(version: 2019_04_02_045144) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "profile_picture"
+    t.string "user_id"
+    t.string "username"
     t.boolean "manager", default: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "spaces"
